8e1c2e8076a0378f649e68ed7d0cab473ce6234f
Harun <harun.1996.187@gmail.com>
2017-01-11 08:55:56 +0200

    Update register.php

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ if(!empty($_POST['email']) && !empty($_POST['password'])):
     $stmt = $conn->prepare($sql);
 
     $stmt->bindParam(':email', $_POST['email']);
-    $stmt->bindParam(':password', password_hash($_POST['password'],PASSWORD_BCRYPT));
+    $stmt->bindParam(':password', password_hash($_POST['password'],PASSWORD_BCRYPT));  // This is not working.
 
     if( $stmt->execute() ):
         $message='Successfully created new user';
@@ -43,7 +43,7 @@ endif;
 
     <?php if(!empty($message)); ?>
         <p><?= $message ?></p>
-    <?php endif; ?>
+    <?php endif; ?>  // This is not working.
 
     <h1>Register</h1>
 
